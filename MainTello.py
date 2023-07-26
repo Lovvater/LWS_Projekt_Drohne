c9b1a9e32090697ab4d0bc767cace43715517581
Lovvater <140611199+Lovvater@users.noreply.github.com>
2023-07-26 13:26:51 +0200

    Update MainTello.py

--- MainTello.py
+++ MainTello.py
@@ -3,6 +3,15 @@ import time
 import KeyboardTelloModule as kp
 import cv2
 
+# Function to perform face recognition
+def detect_faces(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    return image
+
 def getKeyboardInput():
     # LEFT RIGHT, FRONT BACK, UP DOWN, YAW VELOCITY
     lr, fb, ud, yv = 0, 0, 0, 0
@@ -11,35 +20,56 @@ def getKeyboardInput():
     moveSpeed = 85
     rotationSpeed = 100
 
-    if kp.getKey("LEFT"): lr = -speed # Controlling The Left And Right Movement
+    if kp.getKey("LEFT"):
+        lr = -speed
+    elif kp.getKey("RIGHT"):
+        lr = speed
 
-    elif kp.getKey("RIGHT"):   lr = speed
+    if kp.getKey("UP"):
+        fb = moveSpeed
+    elif kp.getKey("DOWN"):
+        fb = -moveSpeed
 
-    if kp.getKey("UP"):  fb = moveSpeed # Controlling The Front And Back Movement
+    if kp.getKey("w"):
+        ud = liftSpeed
+    elif kp.getKey("s"):
+        ud = -liftSpeed
 
-    elif kp.getKey("DOWN"): fb = -moveSpeed
+    if kp.getKey("d"):
+        yv = rotationSpeed
+    elif kp.getKey("a"):
+        yv = -rotationSpeed
 
-    if kp.getKey("w"):  ud = liftSpeed # Controlling The Up And Down Movement
+    if kp.getKey("q"):
+        Drone.land()
+        time.sleep(3)
+    elif kp.getKey("e"):
+        Drone.takeoff()
 
-    elif kp.getKey("s"):  ud = -liftSpeed
-
-    if kp.getKey("d"): yv = rotationSpeed # Controlling the Rotation
-
-    elif kp.getKey("a"):  yv = -rotationSpeed
-
-    if kp.getKey("q"):  Drone.land(); time.sleep(3) # Landing The Drone
-
-    elif kp.getKey("e"):  Drone.takeoff() # Take Off The Drone
-
-    if kp.getKey("z"):  # Screen Shot Image From The Camera Display
+    if kp.getKey("z"):
+        # Screen Shot Image From The Camera Display
         cv2.imwrite(f"tellopy/Resources/Images/{time.time()}.jpg", img)
         time.sleep(0.3)
 
-    elif kp.getKey("f"):    flip(r)
+    if kp.getKey("f"):
+        # Trigger Flip
+        flip("f")
 
-    return [lr, fb, ud, yv]  # Return The Given Value
+    if kp.getKey("g"):
+        # Trigger Flip
+        flip("b")
 
-def flip(direction): Drone.flip(direction)    #r,l,f,b als Flip-Richtung mit angeben
+    if kp.getKey("h"):
+            # Trigger Flip
+        flip("r")
+
+    if kp.getKey("j"):
+            # Trigger Flip
+        flip("l")
+
+    return [lr, fb, ud, yv]
+
+def flip(Richtung): Drone.flip(Richtung)
 
 # Initialize Keyboard Input
 kp.init()
@@ -48,9 +78,6 @@ kp.init()
 Drone = tello.Tello()
 Drone.connect()
 
-# Batteriestatus abrufen und ausgeben
-battery_percentage = Drone.get_battery()
-print(f"Batteriestatus: {battery_percentage}%")
 
 # Start Camera Display Stream
 Drone.streamon()
@@ -62,7 +89,14 @@ while True:
     Drone.send_rc_control(keyValues[0], keyValues[1], keyValues[2], keyValues[3])
     # Get Frame From Drone Camera
     img = Drone.get_frame_read().frame
-    img = cv2.resize(img, (1080, 720))
+    img = cv2.resize(img, (720, 480))
+
+    # Perform Face Recognition
+    img_with_faces = detect_faces(img)
+
     # Show The Frame
-    cv2.imshow("DroneCapture", img)
+    cv2.imshow("DroneCapture", img_with_faces)
     cv2.waitKey(1)
+
+    battery_percentage = Drone.get_battery()
+    print(f"Batteriestatus: {battery_percentage}%")
